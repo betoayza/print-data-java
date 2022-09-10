b1c797669fb0aae57d53803e0e994d6a3f960389
betoayza <aaayza@gmail.com>
2022-09-10 17:20:05 -0300

    Completed

--- src/com/testing/Main.java
+++ src/com/testing/Main.java
@@ -1,7 +1,27 @@
 package com.testing;
 
-public class Main {
-    public static void main(String[] args){
+import java.util.ArrayList;
+import java.util.List;
 
+public class Main {
+
+    public static void main(String[] args){
+        int number=123;
+        float number2=123.123f;
+        double number3=123.123d;
+        boolean toggle=false;
+        long number4=123l;
+
+        List<Object> dataList = new ArrayList<Object>();
+
+        dataList.add(number);
+        dataList.add(number2);
+        dataList.add(number3);
+        dataList.add(toggle);
+        dataList.add(number4);
+
+        for(Object data : dataList){
+            System.out.println(data);
+        }
     }
 }
